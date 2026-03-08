eec093b440e1ab4ea77e7b70fd1fbae6b10289a0
Giorgi <Giorgi@NotAnEmail.com>
2026-03-09 00:56:59 +0400

    genesis

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { Pool } from 'pg';
+
+export const pool = new Pool({
+    connectionString: process.env.DB_URL
+});
+
+export const dbConnection = async () => {
+    try {
+        await pool.connect();
+        console.log("Connected to db.");
+    } catch (error) {
+        console.log(`Connection failed: ${error}`);
+        process.exit(1);
+    }
+};
