3943addd747f6cb5123f996aeb0153400e1460f5
RaketaG <119808656+RaketaG@users.noreply.github.com>
2026-03-14 14:21:29 +0400

    Fix file paths for SSL certificate and key

--- laverna.ts
+++ laverna.ts
@@ -3,8 +3,8 @@ import fs from 'node:fs';
 import { app } from "./src/app.js";
 
 https.createServer({
-    key: fs.readFileSync("./.openssl/laverna.pem"),
-    cert: fs.readFileSync("./.openssl/laverna.crt"),
+    key: fs.readFileSync("./openssl/laverna.pem"),
+    cert: fs.readFileSync("./openssl/laverna.crt"),
 }, app).listen(443, () => {
     console.log("Running");
 });
